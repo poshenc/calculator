16c7526be46b89b057294ed811e97f08a9a56ec5
johnson <poshenc@gmail.com>
2024-05-24 21:06:26 +0800

    calculator layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css';
 import Button from './components/Button';
 
+const keys = ['C', 'DEL', '÷', 'x', '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '=', '0', '.']
+
 function App() {
   return (
-    <Button></Button>
+    <div className='container'>
+      <div className='result'>result</div>
+      {keys.map(key => (
+        <Button key={key} value={key}></Button>
+      ))}
+    </div>
   );
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,14 @@
-const Button = () => {
+const Button = ({
+  value
+}: {
+  value: string;
+}) => {
+  let btnClass = 'button'
+  if (value === '=') btnClass += ' equal'
+  if (value === '0') btnClass += ' zero'
+
   return (
-    <div>Button</div>
+    <div className={btnClass}>{value}</div>
   )
 }
 
